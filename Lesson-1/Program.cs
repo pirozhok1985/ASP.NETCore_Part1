46ea1ffe024e04cb10159f5e4bada9e241819160
Pirozhok1985 <pirozhok1985@gmail.com>
2021-12-16 09:28:44 +1000

    Отображение на стартовой страничке проекта кастомной конфигурации

--- Lesson-1/Program.cs
+++ Lesson-1/Program.cs
@@ -1,6 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+var greetings = app.Configuration["CustomGreetings"];
+app.MapGet("/", () => greetings);
 
 app.Run();
