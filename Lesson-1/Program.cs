e886cfe4fb3a05cddb6c9350707bbfb691cccb77
Pirozhok1985 <pirozhok1985@gmail.com>
2021-12-16 09:35:29 +1000

    Динамическое отслеживание изменений в файле конфигурации

--- Lesson-1/Program.cs
+++ Lesson-1/Program.cs
@@ -1,7 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-var greetings = app.Configuration["CustomGreetings"];
-app.MapGet("/", () => greetings);
+app.MapGet("/", () => app.Configuration["CustomGreetings"]);
 
 app.Run();
