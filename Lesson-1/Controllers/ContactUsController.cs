efe01377546c2a4fd2ae49bddfa1edafbafc24dd
Pirozhok1985 <pirozhok1985@gmail.com>
2021-12-21 10:46:55 +1000

    Добавил контроллер и представление ContactUs

--- Lesson-1/Controllers/ContactUsController.cs
+++ Lesson-1/Controllers/ContactUsController.cs
@@ -6,7 +6,7 @@ namespace WebStore.Controllers
     {
         public IActionResult Index()
         {
-            return View();
+            return View("~/Views/ContactUs/Index.cshtml");
         }
     }
 }
