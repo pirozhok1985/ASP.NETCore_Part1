2fa5f535e775c672d992b3c207e84e336d645567
Pirozhok1985 <pirozhok1985@gmail.com>
2021-12-21 10:21:45 +1000

    Добавил контроллер и придставления для блогов

---
+++ Lesson-1/Controllers/BlogsController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebStore.Controllers
+{
+    public class BlogsController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.Title = "Blog List";
+            return View("~/Views/Blogs/Index.cshtml");
+        }
+
+        public IActionResult Blog()
+        {
+            ViewBag.Title = "Blog Single";
+            return View("~/Views/Blogs/Blog.cshtml");
+        }
+    }
+}
