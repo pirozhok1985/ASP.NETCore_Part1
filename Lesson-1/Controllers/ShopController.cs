6ba1d82698bdf939f68d67e0af7b6b810fa3b815
Pirozhok1985 <pirozhok1985@gmail.com>
2021-12-21 10:56:03 +1000

    Добавил представление и контроллер Shop без SubMenu

---
+++ Lesson-1/Controllers/ShopController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebStore.Controllers
+{
+    public class ShopController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("~/Views/Shop/Index.cshtml");
+        }
+    }
+}
