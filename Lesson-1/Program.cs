d13d728d8c6f247a622059b4d79eb021d9147ba3
Pirozhok1985 <pirozhok1985@gmail.com>
2021-12-22 12:14:28 +1000

    Расширил соглашения MVC тестовым соглашением

--- Lesson-1/Program.cs
+++ Lesson-1/Program.cs
@@ -1,5 +1,10 @@
+using WebStore.Infrastructure.Conventions;
+
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddControllersWithViews();
+builder.Services.AddControllersWithViews(param =>
+{
+    param.Conventions.Add(new TestConvention());
+});
 
 var app = builder.Build();
 
