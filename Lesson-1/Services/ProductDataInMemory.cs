1099d22318cf05089b3958eb5260145a88fa77b8
Pirozhok1985 <pirozhok1985@gmail.com>
2021-12-28 09:06:50 +1000

    Добавил реализацию интерфейса IProductData - ProductDataInMemory

---
+++ Lesson-1/Services/ProductDataInMemory.cs
@@ -0,0 +1,14 @@
+﻿using WebStore.Data;
+using WebStore.Domain.Entities;
+using WebStore.Services.Interfaces;
+
+namespace WebStore.Services;
+
+public class ProductDataInMemory : IProductData
+{
+    public IEnumerable<Brand> GetBrand() => TestData.Brands;
+
+
+    public IEnumerable<Section> GetSections() => TestData.Sections;
+}
+
