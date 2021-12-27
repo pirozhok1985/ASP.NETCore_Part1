4fa6d580bef271bc828af789b2fa41c95c1fdf03
Pirozhok1985 <pirozhok1985@gmail.com>
2021-12-27 20:46:10 +1000

    Создал проект, задача которого описывать все предметные области интернет-магазина. Создал базовые интерфейсы и абстрактные классы

---
+++ WebStore.Domain/Entities/Base/Entity.cs
@@ -0,0 +1,8 @@
+﻿using WebStore.Domain.Entities.Base.Interfaces;
+
+namespace WebStore.Domain.Entities.Base;
+
+public class Entity : IEntity
+{
+    public int Id { get; set; }
+}
---
+++ WebStore.Domain/Entities/Base/NamedEntity.cs
@@ -0,0 +1,8 @@
+﻿using WebStore.Domain.Entities.Base.Interfaces;
+
+namespace WebStore.Domain.Entities.Base;
+
+public class NamedEntity : Entity, INamedEntity
+{
+    public string Name { get; set; }
+}
