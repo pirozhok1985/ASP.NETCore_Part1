4c76e5060fdbd1f685599b08ca39edae7e1ed615
Pirozhok1985 <pirozhok1985@gmail.com>
2021-12-27 21:01:05 +1000

    Добавил интерфейс сервиса каталога товаров

---
+++ Lesson-1/Services/Interfaces/IProductData.cs
@@ -0,0 +1,10 @@
+﻿using WebStore.Domain.Entities;
+
+namespace WebStore.Services.Interfaces;
+
+public interface IProductData
+{
+    public IEnumerable<Brand> GetBrand();
+    public IEnumerable<Section> GetSections();
+}
+
