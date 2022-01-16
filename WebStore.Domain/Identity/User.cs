0a808c6db066432eba24c5eff5d60ab03b842438
Евгений Анисимов <pirozhok1985@gmail.com>
2022-01-16 16:58:29 +0300

    Добавили в класс Role константные поля Administrator и DefaultAdminPassword

--- WebStore.Domain/Identity/User.cs
+++ WebStore.Domain/Identity/User.cs
@@ -4,5 +4,6 @@ namespace WebStore.Domain.Identity;
 
 public class User : IdentityUser
 {
-
+    public const string Administrator = "Admin";
+    public const string DefaultAdminPassword = "Gthtufh_123";
 }
