3cbd9ecb27498f1804496b030b401b14221f64eb
Евгений Анисимов <pirozhok1985@gmail.com>
2022-01-17 11:35:41 +0300

    Настройка отображения меню в заголовке главной страницы, используя визуальный компонент.

---
+++ Lesson-1/Components/UserInfoViewComponent.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebStore.Domain.Identity;
+
+namespace WebStore.Components;
+
+public class UserInfoViewComponent : ViewComponent
+{
+    public IViewComponentResult Invoke() => User.Identity?.IsAuthenticated == true ? View("UserInfo") : View();
+}
