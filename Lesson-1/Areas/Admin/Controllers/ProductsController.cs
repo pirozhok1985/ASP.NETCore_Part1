a5638d51145b0f56d92129d4df444fd19da973da
Pirozhok1985 <pirozhok1985@gmail.com>
2022-01-20 16:40:57 +0300

    Разметка таблицы каталога товаров

--- Lesson-1/Areas/Admin/Controllers/ProductsController.cs
+++ Lesson-1/Areas/Admin/Controllers/ProductsController.cs
@@ -1,11 +1,28 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using WebStore.Domain.Identity;
+using WebStore.Infrastructure.Mappers;
+using WebStore.Services.Interfaces;
 
 namespace WebStore.Areas.Admin.Controllers;
 
 [Area("Admin"), Authorize(Roles = Role.Administrators)]
 public class ProductsController : Controller
 {
-    public IActionResult Index() => View();
+    private readonly IProductData _productData;
+    private readonly ILogger<ProductsController> _logger;
+
+    public ProductsController(IProductData productData, ILogger<ProductsController> logger)
+    {
+        _productData = productData;
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        var productView = _productData.GetProducts().ToView();
+        return  View(productView);
+    }
+
+    
 }
