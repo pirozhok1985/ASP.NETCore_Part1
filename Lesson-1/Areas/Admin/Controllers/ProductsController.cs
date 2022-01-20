8843147c0ca5b844530c7fd0ec9f634f85e07620
Pirozhok1985 <pirozhok1985@gmail.com>
2022-01-20 16:06:29 +0300

    Контроллер каталога товаров

---
+++ Lesson-1/Areas/Admin/Controllers/ProductsController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using WebStore.Domain.Identity;
+
+namespace WebStore.Areas.Admin.Controllers;
+
+[Area("Admin"), Authorize(Roles = Role.Administrators)]
+public class ProductsController : Controller
+{
+    public IActionResult Index() => View();
+}
