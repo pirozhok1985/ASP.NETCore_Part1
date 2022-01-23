4b24ad3c749d102547c0360b9c14397338d8ce06
anisimov evgenii <pirozhok1985@gmail.com>
2022-01-23 15:49:55 +0300

    Логика работы контроллера корзины при формировании заказа

--- Lesson-1/Controllers/CartController.cs
+++ Lesson-1/Controllers/CartController.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using WebStore.Services.Interfaces;
+using WebStore.ViewModels;
 
 namespace WebStore.Controllers;
 
@@ -8,7 +10,7 @@ public class CartController : Controller
     private readonly ICartService _cartService;
 
     public CartController(ICartService cartService) => _cartService = cartService;
-    public IActionResult Index() => View(_cartService.GetCartViewModel());
+    public IActionResult Index() => View(new CartOrderViewModel{CartViewModel = _cartService.GetCartViewModel()});
 
     public IActionResult Add(int id)
     {
@@ -24,5 +26,30 @@ public class CartController : Controller
     {
         _cartService.Decrement(id);
         return RedirectToAction("Index", "Cart");
+    }
+
+    [Authorize,HttpPost,ValidateAntiForgeryToken]
+    public async Task<IActionResult> CheckOut(OrderViewModel orderViewModel, [FromServices] IOrderService orderService)
+    {
+        if (!ModelState.IsValid)
+            return View(nameof(Index), new CartOrderViewModel
+            {
+                CartViewModel = _cartService.GetCartViewModel(),
+                OrderViewModel = orderViewModel
+            });
+        var order = await orderService.CreateNewOrder
+        (
+            User.Identity.Name,
+            _cartService.GetCartViewModel(),
+            orderViewModel
+        );
+        _cartService.Clear();
+        return RedirectToAction(nameof(OrderConfirmed), new {order.Id});
+    }
+
+    public IActionResult OrderConfirmed(int orderId)
+    {
+        ViewBag.OrderId = orderId;
+        return View();
     }
 }
