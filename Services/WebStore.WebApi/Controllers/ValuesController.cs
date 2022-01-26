d5cfa83f825e6a38c6836487a18d16da3d12e47c
anisimov evgenii <pirozhok1985@gmail.com>
2022-01-26 19:17:11 +0300

    Проект-хостинг для нашего WebAPI

---
+++ Services/WebStore.WebApi/Controllers/ValuesController.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebStore.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+    }
+}
