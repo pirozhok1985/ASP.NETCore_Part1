7a5effd5a17dc2f143a229882dc4a0b30baa646a
anisimov evgenii <pirozhok1985@gmail.com>
2022-01-28 18:33:09 +0300

    Регистрация сервисов бизнесс логики в проекте хоста WebAPI

--- Services/WebStore.WebApi/Program.cs
+++ Services/WebStore.WebApi/Program.cs
@@ -1,3 +1,12 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using WebStore.DAL.Context;
+using WebStore.Domain.Identity;
+using WebStore.Interfaces.Services;
+using WebStore.Services.Services;
+using WebStore.Services.Services.Cookies;
+using WebStore.Services.Services.Database;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -8,6 +17,32 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+builder.Services.AddDbContext<WebStoreDB>(opt => opt.UseSqlServer(builder.Configuration.GetConnectionString("SqlServer")));
+builder.Services.AddTransient<IDbInitializer, DbInitializer>();
+builder.Services.AddIdentity<User, Role>(opt =>
+{
+#if DEBUG
+    opt.Password.RequireDigit = false;
+    opt.Password.RequireLowercase = false;
+    opt.Password.RequireUppercase = false;
+    opt.Password.RequireDigit = false;
+    opt.Password.RequireNonAlphanumeric = false;
+    opt.Password.RequiredLength = 3;
+    opt.Password.RequiredUniqueChars = 3;
+#endif
+    opt.User.AllowedUserNameCharacters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    opt.User.RequireUniqueEmail = false;
+
+    opt.Lockout.AllowedForNewUsers = false;
+    opt.Lockout.MaxFailedAccessAttempts = 10;
+    opt.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(15);
+
+}).AddEntityFrameworkStores<WebStoreDB>().AddDefaultTokenProviders();
+
+builder.Services.AddScoped<IEmployeesData, EmployeeDataDB>();
+builder.Services.AddScoped<IProductData, ProductDataDB>();
+// builder.Services.AddScoped<ICartService,CartServiceCookies>();
+builder.Services.AddScoped<IOrderService, OrderServiceDB>();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
