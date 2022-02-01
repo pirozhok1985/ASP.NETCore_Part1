4c1b7a449dea513158a25cbce33982f89704e0ea
Evgenii Anisimov <pirozhok1985@gmail.com>
2022-02-01 17:07:40 +0300

    DTO - модели для WebApi системы Identity

---
+++ Common/WebStore.Domain/DTO/Identity/ClaimDto.cs
@@ -0,0 +1,20 @@
+﻿using System.Security.Claims;
+
+namespace WebStore.Domain.DTO.Identity;
+
+public abstract class ClaimDto : UserDto
+{
+    public IEnumerable<Claim> Claims { get; set; }
+}
+
+public class AddClaimDto : ClaimDto
+{
+}
+public class RemoveClaimDto : ClaimDto
+{
+}
+public class ReplaceClaimDto : UserDto
+{
+    public Claim OldClaim { get; set; }
+    public Claim NewClaim { get; set; }
+}
---
+++ Common/WebStore.Domain/DTO/Identity/UserDto.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Identity;
+using WebStore.Domain.Identity;
+
+namespace WebStore.Domain.DTO.Identity;
+
+public abstract class UserDto
+{
+    public User User { get; set; }
+}
+
+public class LoginDto : UserDto
+{
+    public UserLoginInfo LoginInfo { get; set; }
+}
+
+public class PasswordHashDto : UserDto
+{
+    public string Hash { get; set; }
+}
+
+public class LockOutDto : UserDto
+{
+    public DateTimeOffset LockoutEndTime { get; set; }
+}
