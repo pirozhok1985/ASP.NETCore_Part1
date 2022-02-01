2f92c9a4335a5aeab38114efd977c32082a5a60f
Evgenii Anisimov <pirozhok1985@gmail.com>
2022-02-01 15:18:46 +0300

    Интерфейсы, требующие реализации

---
+++ Services/WebStore.Interfaces/Services/Identity/IUserClient.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity;
+using WebStore.Domain.Identity;
+
+namespace WebStore.Interfaces.Services.Identity;
+
+public interface IUserClient :
+    IUserClaimStore<User>,
+    IUserEmailStore<User>,
+    IUserLockoutStore<User>,
+    IUserLoginStore<User>,
+    IUserPasswordStore<User>,
+    IUserRoleStore<User>,
+    IUserTwoFactorStore<User>,
+    IUserPhoneNumberStore<User>
+{
+    
+}
