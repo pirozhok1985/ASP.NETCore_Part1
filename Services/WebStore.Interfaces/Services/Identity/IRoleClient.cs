98ac8f495b2c4ce955583759376271a2488bdc03
Evgenii Anisimov <pirozhok1985@gmail.com>
2022-02-03 21:30:55 +0300

    Подготовили интерфейсы для системы Identity

---
+++ Services/WebStore.Interfaces/Services/Identity/IRoleClient.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity;
+using WebStore.Domain.Identity;
+
+namespace WebStore.Interfaces.Services.Identity;
+
+public interface IRoleClient : IRoleStore<Role>
+{
+}
---
+++ Services/WebStore.Interfaces/Services/Identity/IUserClient.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity;
+using WebStore.Domain.Identity;
+
+namespace WebStore.Interfaces.Services.Identity;
+
+public interface IUserClient :
+    IUserLockoutStore<User>,
+    IUserClaimStore<User>,
+    IUserLoginStore<User>,
+    IUserEmailStore<User>,
+    IUserPasswordStore<User>,
+    IUserPhoneNumberStore<User>,
+    IUserTwoFactorStore<User>,
+    IUserRoleStore<User>
+{
+    
+}
