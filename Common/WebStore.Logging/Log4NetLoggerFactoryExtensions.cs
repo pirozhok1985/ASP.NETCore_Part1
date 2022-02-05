ceb8ccf1d916ba4fdcf7608bd2eb3e5bee2fb3c5
Evgenii Anisimov <pirozhok1985@gmail.com>
2022-02-05 16:09:21 +0300

    Инфраструктура для добавления log4net в систему логирования

---
+++ Common/WebStore.Logging/Log4NetLoggerFactoryExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System.Configuration;
+using System.Reflection;
+using Microsoft.Extensions.Logging;
+
+namespace WebStore.Logging;
+
+public static class Log4NetLoggerFactoryExtensions
+{
+    private static string CheckFilePath(string filePath)
+    {
+        if (filePath == string.Empty)
+            throw new ArgumentException("Не указан путь к файлу конфигурации");
+        return Path.IsPathRooted(filePath) 
+            ? filePath 
+            : Path.Combine(Path.GetDirectoryName(Assembly.GetEntryAssembly()!.Location)!, filePath);
+    }
+
+    public static ILoggingBuilder AddLog4Net(this ILoggingBuilder builder, string configurationFile = "log4net.config")
+    {
+        return builder.AddProvider(new Log4NetLoggerProvider(CheckFilePath(configurationFile)));
+    }
+}
---
+++ Common/WebStore.Logging/Log4NetLoggerProvider.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Concurrent;
+using System.Xml;
+using Microsoft.Extensions.Logging;
+
+namespace WebStore.Logging;
+
+public class Log4NetLoggerProvider : ILoggerProvider
+{
+    private readonly string _configurationFile;
+    private readonly ConcurrentDictionary<string, Log4NetLogger> _loggers = new();
+
+    public Log4NetLoggerProvider(string configurationFile)
+    {
+        _configurationFile = configurationFile;
+    }
+    public ILogger CreateLogger(string category)
+    {
+        return _loggers.GetOrAdd(category, (categoryName, configFile) =>
+        {
+            var xml = new XmlDocument();
+            xml.LoadXml(configFile);
+            return new Log4NetLogger(categoryName, xml["log4net"]!);
+        }, _configurationFile);
+        
+    }
+
+    public void Dispose() => _loggers.Clear();
+}
