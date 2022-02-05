b5ee9918ad801c6c75ce284c08f55b89bfe55bf6
Evgenii Anisimov <pirozhok1985@gmail.com>
2022-02-05 13:02:32 +0300

    Логгирование контроллера Account

--- Services/WebStore.Services/WebAddresses.cs
+++ Services/WebStore.Services/WebAddresses.cs
@@ -6,6 +6,7 @@ public static class WebAddresses
     public const string Products = "api/v1/products";
     public const string Employees = "api/v1/employees";
     public const string Values = "api/v1/values";
+    public const string Console = "api/v1/console";
 
     public static class Identity
     {
