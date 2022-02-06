dc986765ad2a302006b4272d0709886861c610b1
Evgenii Anisimov <pirozhok1985@gmail.com>
2022-02-06 14:46:13 +0300

    Тестирование с использованием данных из других сервисов

---
+++ Tests/Webstore.Tests/WebApiControllerTests.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using WebStore.Controllers;
+using WebStore.Interfaces.TestAPI;
+using Assert = Xunit.Assert;
+
+namespace Webstore.Tests;
+
+[TestClass]
+public class WebApiControllerTests
+{
+    [TestMethod]
+    public void IndexReturnsViewWithData()
+    {
+        var data = Enumerable.Range(1, 10).Select(v => $"Value-{v}").ToArray();
+        var valueServiceMock = new Mock<IValueService>();
+        valueServiceMock.Setup(s => s.GetValues()).Returns(data);
+        var controller = new WebApiController(valueServiceMock.Object);
+
+        var result = controller.Index();
+        var viewResult = Assert.IsType<ViewResult>(result);
+        var model = Assert.IsAssignableFrom<IEnumerable<string>>(viewResult.Model);
+        var i = 0;
+        foreach (var actualResult in model)
+        {
+            var expectedResult = data[i++];
+            Assert.Equal(expectedResult,actualResult);
+        }
+        valueServiceMock.Verify(s => s.GetValues());
+        valueServiceMock.VerifyNoOtherCalls();
+    }
+}
