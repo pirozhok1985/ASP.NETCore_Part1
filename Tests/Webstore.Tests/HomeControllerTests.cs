f1e21101dead9450d3a4546621170989277b3016
Evgenii Anisimov <pirozhok1985@gmail.com>
2022-02-06 13:54:05 +0300

    Первый модульный тест с использованием фейковой реализацией интерфейса

---
+++ Tests/Webstore.Tests/HomeControllerTests.cs
@@ -0,0 +1,38 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using WebStore.Controllers;
+using WebStore.Domain;
+using WebStore.Domain.Entities;
+using WebStore.Interfaces.Services;
+using Assert = Xunit.Assert;
+
+namespace Webstore.Tests;
+
+[TestClass]
+public class HomeControllerTests
+{
+    [TestMethod]
+    public void IndexReturnsView()
+    {
+        var productDataMock = new Mock<IProductData>();
+        productDataMock.Setup(p => p.GetProducts(It.IsAny<ProductFilter>()))
+            .Returns(Enumerable.Empty<Product?>());
+
+        var actualResult = new HomeController().Index(productDataMock.Object);
+        Assert.IsType<ViewResult>(actualResult);
+    }
+
+    [TestMethod]
+    public void CustomActionReturnsContent()
+    {
+        const string id = "123";
+        var controller = new HomeController();
+        
+        var resultValue = controller.CustomAction(id);
+        
+        Assert.IsType<ContentResult>(resultValue);
+    }
+
+}
