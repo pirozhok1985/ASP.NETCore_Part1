49d099ac2bc76f15d9fd5f6f0ae2f5ade8e0b7e8
Evgenii Anisimov <pirozhok1985@gmail.com>
2022-02-12 23:14:23 +0300

    Хлебные крошки в представлении каталога товаров и детализации товара

--- UI/WebStore/Components/BreadCrumbsViewComponent.cs
+++ UI/WebStore/Components/BreadCrumbsViewComponent.cs
@@ -1,8 +1,38 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WebStore.Domain.ViewModels;
+using WebStore.Interfaces.Services;
 
 namespace WebStore.Components;
 
 public class BreadCrumbsViewComponent : ViewComponent
 {
-    IViewComponentResult Invoke() => View();
+    private readonly IProductData _productData;
+
+    public BreadCrumbsViewComponent(IProductData productData) => _productData = productData;
+
+    public IViewComponentResult Invoke()
+    {
+        var breadCrumbs = new BreadCrumbsViewModel();
+        if (int.TryParse(HttpContext.Request.Query["sectionId"], out var sectionId))
+        {
+            breadCrumbs.Section = _productData.GetSectionById(sectionId);
+            if (breadCrumbs.Section!.ParentId is { } sectionParentId && breadCrumbs.Section.Parent is null)
+                breadCrumbs.Section.Parent = _productData.GetSectionById(sectionParentId);
+        }
+
+        if (int.TryParse(HttpContext.Request.Query["brandId"], out var brandId))
+        {
+            breadCrumbs.Brand = _productData.GetBrandById(brandId);
+        }
+
+
+        if (int.TryParse(HttpContext.Request.RouteValues["id"]?.ToString(), out var productId))
+        {
+            breadCrumbs.ProductName = _productData.GetProductById(productId)!.Name;
+        }
+
+        return View(breadCrumbs);
+    }
+
+
 }
