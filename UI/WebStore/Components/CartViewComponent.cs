9a7b9ee49d6d0b153da2bf0034a80394d81798bf
Evgenii Anisimov <pirozhok1985@gmail.com>
2022-02-13 11:37:08 +0300

    Визуальный компонент корзины в заголовке страницы

---
+++ UI/WebStore/Components/CartViewComponent.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebStore.Interfaces.Services;
+
+namespace WebStore.Components;
+
+public class CartViewComponent : ViewComponent
+{
+    private readonly ICartStore _cartStore;
+
+    public CartViewComponent(ICartStore cartStore)
+    {
+        _cartStore = cartStore;
+    }
+    public IViewComponentResult Invoke()
+    {
+        ViewBag.Count = _cartStore.Cart.ItemsSum;
+        return View();
+    } 
+}
